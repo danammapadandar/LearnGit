5d90955e08ebba87a84397fc7a9f0db99514b17f
danammapadandar <154652836+danammapadandar@users.noreply.github.com>
2026-02-12 20:09:22 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,38 @@
+
+integer_num = 10
+float_num = 10.5
+complex_num = 2 + 3j
+
+
+boolean_val = True
+
+
+string_val = "Hello Python"
+
+
+list_val = [1, 2, 3, 4]
+tuple_val = (5, 6, 7)
+range_val = range(1, 5)
+
+
+set_val = {1, 2, 3}
+frozenset_val = frozenset({4, 5, 6})
+
+
+dict_val = {"name": "Danu", "branch": "CSE"}
+
+
+none_val = None
+
+print("Integer:", integer_num, type(integer_num))
+print("Float:", float_num, type(float_num))
+print("Complex:", complex_num, type(complex_num))
+print("Boolean:", boolean_val, type(boolean_val))
+print("String:", string_val, type(string_val))
+print("List:", list_val, type(list_val))
+print("Tuple:", tuple_val, type(tuple_val))
+print("Range:", list(range_val), type(range_val))
+print("Set:", set_val, type(set_val))
+print("FrozenSet:", frozenset_val, type(frozenset_val))
+print("Dictionary:", dict_val, type(dict_val))
+print("None:", none_val, type(none_val))
